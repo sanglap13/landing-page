fe123d4e69fe40f80e4490d2a97cf5ec441d358d
mythop13 <smridha@aeonixinnovations.com>
2024-02-28 19:07:51 +0530

    Initial and Final Commit!

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        brightRed: "#f25f3b",
+        brightRedLight: "#f6866a",
+        brightRedSupLight: "#fdece7",
+        darkBlue: "#242d52",
+        darkGrayishBlue: "#9095a7",
+        veryDarkBlue: "#1d1e25",
+        veryPaleRed: "#ffefeb",
+        veryLightGray: "#fafafa",
+      },
+    },
+  },
+  plugins: [],
+};
